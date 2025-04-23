6320f268fc1e3284e2007bc26a2d0a6d4fe0ee3b
BHorner800 <benjamin.r.horner@gmail.com>
2025-04-23 10:37:31 -0400

    Create HW12Code.R

---
+++ HW12Code.R
@@ -0,0 +1,21 @@
+#####################################################
+#HW12
+# Ben Horner
+# MATH 240
+#####################################################
+##################################
+#Libraries
+##################################
+library(tidyverse)
+
+
+##################################
+# Question 1
+##################################
+#values of t20 that provide support
+
+
+#Values of t30 that provide support
+
+
+#Simulation Study for Type I error
